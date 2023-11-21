58a55a953238e4f90d941b54d97916e2f5aa9fdd
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2023-11-21 16:55:16 +0100

    Initial implementation of new classes

--- 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/controller/DrawNumberControllerMultipleViews.java
+++ 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/controller/DrawNumberControllerMultipleViews.java
@@ -1,5 +1,43 @@
 package it.unibo.mvc.controller;
 
-public class DrawNumberControllerMultipleViews  {
-    
+import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
+
+import java.util.Collection;
+
+public class DrawNumberControllerMultipleViews implements DrawNumberController {
+
+    Collection<DrawNumberView> views; /* Views collection, so the controller knows how many view it has to control */
+
+    /**
+     * Makes a guess.
+     * 
+     * @param n the attempt
+     */
+    public void newAttempt(int n){
+
+    }
+
+    /**
+     * Resets the current game (if any is running) and starts a new one.
+     */
+    public void resetGame(){
+
+    }
+
+    /**
+     * Gracefully quits from the application.
+     */
+    public void quit(){
+
+    }
+
+    /**
+     * Adds a new view.
+     *
+     * @param newView the view to be added
+     */
+    public void addView(DrawNumberView newView){
+
+    }
 }
--- 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/view/DrawNumberConsoleView.java
+++ 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/view/DrawNumberConsoleView.java
@@ -1,7 +1,32 @@
 package it.unibo.mvc.view;
 
+import it.unibo.mvc.api.DrawNumberController;
 import it.unibo.mvc.api.DrawNumberView;
+import it.unibo.mvc.api.DrawResult;
 
 public class DrawNumberConsoleView implements DrawNumberView {
-    
+            /**
+     * Sets the controller controlled by this view (if works as input).
+     *
+     * @param observer the controller to attach
+     */
+    public void setController(DrawNumberController observer){
+
+    }
+
+    /**
+     * This method is called before the UI is used. It should finalize its status (if needed).
+     */
+    public void start(){
+        
+    }
+
+    /**
+     * Tells the UI to display the result of the draw.
+     *
+     * @param res the result of the last draw
+     */
+    public void result(DrawResult res){
+
+    }
 }
