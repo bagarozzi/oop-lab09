620c1bcb381692480bdc91635ef1bd297344981f
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2023-11-27 16:53:49 +0100

    Implemented controller interface

--- 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,16 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.util.List;
 /**
  *
  */
 public interface Controller {
+
+    File getCurrentFile();
+
+    String getCurrentFilePath();
+
+    void print();
 
 }
