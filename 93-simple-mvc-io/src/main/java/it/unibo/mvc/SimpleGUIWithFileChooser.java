3cad559ba66c6ed379d0553da1471613466eb494
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2023-11-27 16:35:58 +0100

    Added save button, working browse button and chagend the interface cuz it was wrong

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUIWithFileChooser.java
@@ -11,6 +11,10 @@ import javax.swing.border.Border;
 import javax.swing.JTextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.awt.Dimension;
+import java.awt.TextArea;
+import java.awt.Toolkit;
 
 
 /**
@@ -20,27 +24,73 @@ import java.awt.event.ActionListener;
 public final class SimpleGUIWithFileChooser {
 
     private final JFrame frame = new JFrame();
+    private final int PROPORTION = 5;
     private Controller controller;
 
     public SimpleGUIWithFileChooser(Controller c){
         controller = c;
         JPanel canvas = new JPanel();
         JPanel subcanvas = new JPanel();
-        JTextField tf = new JTextField("Browse");
-        JButton browse = new JButton();
+        JTextField currentFileField = new JTextField();
+        JTextArea textArea = new JTextArea();
+        JButton browse = new JButton("Browse");
+        JButton save = new JButton("Save");
 
         canvas.setLayout(new BorderLayout());
         subcanvas.setLayout(new BorderLayout());
-        subcanvas.add(tf, BorderLayout.CENTER);
-        subcanvas.add(browse, BorderLayout.LINE_END);
-        canvas.add(subcanvas, BorderLayout.NORTH);
 
-        tf.setEditable(false);
-        tf.setText("The current file is: " + controller.getCurrentFile());
+        subcanvas.add(currentFileField, BorderLayout.CENTER);
+        subcanvas.add(browse, BorderLayout.LINE_END);
+
+        canvas.add(textArea, BorderLayout.CENTER);
+        canvas.add(subcanvas, BorderLayout.NORTH);
+        canvas.add(save, BorderLayout.SOUTH);
+
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        currentFileField.setEditable(false);
+        currentFileField.setText("The current file is: " + controller.getCurrentFile());
 
         browse.addActionListener(new ActionListener(){
             public void actionPerformed(final ActionEvent e){
                 JFileChooser jfc = new JFileChooser();
+                switch (jfc.showSaveDialog(canvas)) {
+                    case JFileChooser.APPROVE_OPTION:
+                        controller.setCurrentFile(jfc.getSelectedFile());
+                        currentFileField.setText("The current file is: " + jfc.getSelectedFile().getAbsolutePath());;
+                        break;
+                    case JFileChooser.CANCEL_OPTION:
+                        break;
+                    default:
+                        break;
+                }
+            }
         });
+        
+        save.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                try{
+                    controller.saveContentToFile(textArea.getText());
+                } catch(IOException e1){
+                    System.out.println("There was a problem saving the content or the content passed is null");
+                }
+            }
+        });
+    }
+
+    public void display(){
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args){
+        SimpleGUIWithFileChooser sg = new SimpleGUIWithFileChooser(new Controller());
+        sg.display();
     }
 }
