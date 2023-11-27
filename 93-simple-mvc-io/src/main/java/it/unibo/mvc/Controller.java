52e2e935fc6f37d1637989733ba62cfa52637c56
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2023-11-27 14:59:44 +0100

    Complete implementation of Controller Class

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,34 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    private File currentFile = new File(System.getProperty("user.home") + System.getProperty("file.separataor") + "output.txt");
 
+    public Controller(){
+
+    }
+
+    public void setCurrentFile(File f){
+        currentFile = f;
+    }
+
+    public String getCurrentFile(){
+        return currentFile.getAbsolutePath();
+    }
+
+    public String getCurrentFilePath(){
+        return currentFile.getAbsolutePath();
+    }
+
+    public void saveContentToFile(String content) throws IOException {
+        if(currentFile == null || content == null){
+            throw new IOException("No file has been set or the content passed is null");
+        }
+    }
 }
