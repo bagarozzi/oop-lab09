56365a917a93f56870d20a4ee214084531f99dc2
Federico Bagattoni <federico.bagattoni@studio.unibo.it>
2023-11-27 15:12:21 +0100

    Complete implementation of SimpleGUI

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,13 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +15,27 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private final JFrame frame = new JFrame("Exercise 2");
+    private final int PROPORTION = 5;
 
+    public SimpleGUI(){
+        JPanel canvas = new JPanel();
+        JTextArea textArea = new JTextArea();
+        JButton save = new JButton("Save");
+
+        canvas.setLayout(new BorderLayout());
+        canvas.add(textArea, BorderLayout.CENTER);
+        canvas.add(save, BorderLayout.SOUTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void display(){
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
 }
